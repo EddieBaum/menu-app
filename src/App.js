bf209c83960a3482f4ecea51afea2fca83f266bb
EddieBaum <115045326+EddieBaum@users.noreply.github.com>
2023-07-03 08:28:05 -0500

    add /menu-app to index pathway such that the Homepage component opens when npm start command is given, this may effect how app runs in ghpages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route, Link, Outlet } from "react-router-dom";
-import React, { useState } from "react";
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route, Router, Link, Outlet } from "react-router-dom";
+import React from "react";
 import Homepage from "./Homepage/Homepage";
 import EnglishMenu from "./Menus/EnglishMenu";
 import SpanishMenu from "./Menus/SpanishMenu";
@@ -9,7 +9,7 @@ function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root />}>
-        <Route index element={<Homepage />} />
+        <Route index path='/menu-app' element={<Homepage />} />
         <Route path="/english-menu" element={<EnglishMenu />} />
         <Route path="/spanish-menu" element={<SpanishMenu />} />
       </Route>
